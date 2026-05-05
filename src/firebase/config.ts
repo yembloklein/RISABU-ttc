1878673e373d3d74126e99e51825e010707f83f2
Clain Yemblo <clainyemblo@gmail.com>
2026-05-05 10:01:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6042688875-c5251",
+  "appId": "1:905700458697:web:c0281d8b8cbf4272654d18",
+  "apiKey": "AIzaSyASZ4AhfMW-rUCWLoA3QDAkjfV8gKHmVbM",
+  "authDomain": "studio-6042688875-c5251.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "905700458697"
+};
